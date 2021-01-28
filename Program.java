b99ded9ddeff2a20b76d134b94d2895b7bea9032
Oleksandr Bushovskyi <sasha.bush2001@gmail.com>
2021-01-28 10:20:55 +0200

    Added another new comment

--- Program.java
+++ Program.java
@@ -3,6 +3,7 @@ public class Program
 	public static void main(String[] args){
 		//Added a comment
 		//added a new comment
+		//added another new comment
 		System.out.println("Hello world and something!");
 	}
 }
